f056bce47ccf2a7d05e152c15970b7a534add777
Yauhen Nohach Dev <jenaynogach1@mail.ru>
2025-04-28 16:06:06 +0300

    update property v2

--- src/main/java/com/example/realestaterental/mapper/PropertyMapper.java
+++ src/main/java/com/example/realestaterental/mapper/PropertyMapper.java
@@ -3,6 +3,7 @@ package com.example.realestaterental.mapper;
 import com.example.realestaterental.dto.PropertyDTO;
 import com.example.realestaterental.entity.Photo;
 import com.example.realestaterental.entity.Property;
+import com.example.realestaterental.service.PropertyService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -10,11 +11,14 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Mapper(componentModel = "spring")
+@Mapper(
+        componentModel = "spring",
+        uses = PropertyService.class             // ← добавили
+)
 public interface PropertyMapper {
     @Mapping(target = "hostId", expression = "java(getHostId(property))")
     @Mapping(target = "photos", expression = "java(mapPhotos(property.getPhotos()))")
-    @Mapping(target = "averageRating", expression = "java(propertyService.findAverageReviewForProperty(property.getId()))")
+//    @Mapping(target = "averageRating", expression = "java(propertyService.findAverageReviewForProperty(property.getId()))")
     PropertyDTO toDto(Property property);
 
     default List<String> mapPhotos(List<Photo> photos) {
