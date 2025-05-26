092c6f0338749b808a3e6ae96cdb961566bb3745
ksushoka <makovesova87@mail.ru>
2025-05-26 19:22:39 +0300

    refactor Configuration v.2.8

--- src/main/java/com/example/realestaterental/security/SecurityConfiguration.java
+++ src/main/java/com/example/realestaterental/security/SecurityConfiguration.java
@@ -33,7 +33,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors().and()
+                .cors().disable()
+//                .and()
                 .csrf().disable()
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
